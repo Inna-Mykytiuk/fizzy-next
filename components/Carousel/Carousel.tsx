a0019f575f730f8fe1470d690079ae462873b71f
Inna Mykytiuk <innka-pinnka@ukr.net>
2025-04-29 13:15:24 +0300

    add carousel

--- components/Carousel/Carousel.tsx
+++ components/Carousel/Carousel.tsx
@@ -1,12 +1,17 @@
 "use client";
 
+import { useGSAP } from "@gsap/react";
 import { Center, Environment, View } from "@react-three/drei";
+import gsap from "gsap";
 import React, { useRef, useState } from "react";
 import { Group } from "three";
 
-import { ArrowIcon } from "../ArrowIcon";
+import { ArrowButton } from "../ArrowButton";
 import FloatingCan from "../FloatingCan";
 import { SodaCanProps } from "../UI/SodaCan";
+import { WavyCircles } from "../WavyCircles";
+
+const SPINS_ON_CHANGE = 8;
 
 const FLAVORS: {
   flavor: SodaCanProps["flavor"];
@@ -29,26 +34,56 @@ function Carousel() {
   const sodaCanRef = useRef<Group>(null);
 
   function changeFlavor(index: number) {
-    const nextIndex = (index + 1) % FLAVORS.length;
-    setCurrentFlavorIndex(nextIndex);
+    if (!sodaCanRef.current) return;
+    // Ensure the index is always positive and within range
+    const nextIndex =
+      ((index % FLAVORS.length) + FLAVORS.length) % FLAVORS.length;
+    const tl = gsap.timeline();
+
+    tl.to(
+      sodaCanRef.current.rotation,
+      {
+        y:
+          index > currentFlavorIndex
+            ? `-=${Math.PI * 2 * SPINS_ON_CHANGE}`
+            : `+=${Math.PI * 2 * SPINS_ON_CHANGE}`,
+        ease: "power2.inOut",
+        duration: 1,
+      },
+      0,
+    )
+      .to(
+        ".background, .wavy-circles-outer, .wavy-circles-inner",
+        {
+          backgroundColor: FLAVORS[nextIndex].color,
+          fill: FLAVORS[nextIndex].color,
+          ease: "power2.inOut",
+          duration: 1,
+        },
+        0,
+      )
+      .to(".text-wrapper", { duration: 0.2, y: -10, opacity: 0 }, 0)
+      .to({}, { onStart: () => setCurrentFlavorIndex(nextIndex) }, 0.5)
+      .to(".text-wrapper", { duration: 0.2, y: 0, opacity: 1 }, 0.7);
   }
 
   return (
-    <section className="carousel grid-rows-[auto, 4fr, auto] relative grid h-screen justify-center overflow-hidden bg-white py-12 text-white">
-      <div className="background pointer-events-none absolute inset-0 bg-[#ffa2b6] opacity-50" />
+    <section className="carousel relative grid h-screen grid-rows-[auto,4fr,auto] justify-center overflow-hidden bg-white py-12 text-white">
+      <div className="background pointer-events-none absolute inset-0 bg-[#710523] opacity-50" />
+      <WavyCircles className="absolute top-1/2 left-1/2 h-[100vmin] -translate-x-1/2 -translate-y-1/2 text-[#710523]" />
+
       <h2 className="relative text-center text-5xl font-bold">
-        {" "}
         Choose Your Flavor
       </h2>
 
-      <div className="grid-cols-[auto, auto, auto] grid items-center">
+      <div className="grid w-full grid-cols-3 items-center justify-items-center">
         {/* Left */}
-        <button
+        <ArrowButton
           onClick={() => changeFlavor(currentFlavorIndex + 1)}
-          className="z-20"
-        >
-          Left
-        </button>
+          direction="left"
+          label="Previous Flavor"
+        />
+
         {/* Can */}
         <View className="aspect-square h-[70vmin] min-h-40">
           <Center position={[0, 0, 1.5]}>
@@ -59,7 +94,6 @@ function Carousel() {
               flavor={FLAVORS[currentFlavorIndex].flavor}
             />
           </Center>
-
           <Environment
             files="/hdr/lobby.hdr"
             environmentIntensity={0.6}
@@ -67,10 +101,21 @@ function Carousel() {
           />
           <directionalLight intensity={6} position={[0, 1, 1]} />
         </View>
+
         {/* Right */}
+        <ArrowButton
+          onClick={() => changeFlavor(currentFlavorIndex - 1)}
+          direction="right"
+          label="Next Flavor"
+        />
       </div>
 
-      <p>12 cans - $35.99</p>
+      <div className="text-area relative mx-auto text-center">
+        <div className="text-wrapper text-4xl font-medium">
+          <p>{FLAVORS[currentFlavorIndex].name}</p>
+        </div>
+        <p className="mt-2 text-2xl font-normal opacity-90">12 cans - $35.99</p>
+      </div>
     </section>
   );
 }
